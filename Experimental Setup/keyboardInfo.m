f56510c46cb92defc8ab07e2f30af3d44459eb30
pauLiou <93779312+pauLiou@users.noreply.github.com>
2022-10-05 18:15:31 +0100

    Add files via upload

---
+++ Experimental Setup/keyboardInfo.m
@@ -0,0 +1,16 @@
+% Define the keyboard keys that are listened for. We will be using the left
+% and right arrow keys as response keys for the task and the escape key as
+% a exit/reset key
+escapeKey = KbName('ESCAPE');
+leftKey = KbName('LeftArrow');
+rightKey = KbName('RightArrow');
+downKey = KbName('DownArrow');
+upKey = KbName('UpArrow');
+
+% Pause key
+pause_key = KbName('p');
+
+% Response Keys for Calibration (to accept calibration press 'y', to redo
+% the calibration press any other key)
+accept_key = KbName('y');
+acceptedCalibration = false;
